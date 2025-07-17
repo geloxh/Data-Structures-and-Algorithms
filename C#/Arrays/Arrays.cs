fe317a26f60282621c4d49bc150ff933304a5759
Angelo <ace.blazado7@gmail.com>
2025-07-17 17:25:40 +0800

    New Commit for Data Structures and Algorithms

---
+++ C#/Arrays/Arrays.cs
@@ -0,0 +1,4 @@
+// A collection of elements identified by index or key.
+// Fixed size and can store elements of the same type.
+
+int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
---
+++ Java/Arrays/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        // Initializing array
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        // Size of array
+        int n = arr.length;
+
+        // Traversing array
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    } 
+}
